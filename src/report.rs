9e6475e57ea2715c281a1297c7f4cd3fc516fdce
Eric Hodel <drbrain@segment7.net>
2024-01-18 22:30:01 -0800

    Add soil humidity and indoor sensors
    Fix some labels

--- src/report.rs
+++ src/report.rs
@@ -14,11 +14,27 @@ pub struct Report {
     aqi_pm25_aqin: Option<f64>,
     baromabsin: Option<f64>,
     baromrelin: Option<f64>,
+    batt1: Option<f64>,
+    batt2: Option<f64>,
+    batt3: Option<f64>,
+    batt4: Option<f64>,
+    batt5: Option<f64>,
+    batt6: Option<f64>,
+    batt7: Option<f64>,
+    batt8: Option<f64>,
     batt_25: Option<f64>,
     batt_co2: Option<f64>,
     batt_lightning: Option<f64>,
     battin: Option<f64>,
     battout: Option<f64>,
+    battsm1: Option<f64>,
+    battsm2: Option<f64>,
+    battsm3: Option<f64>,
+    battsm4: Option<f64>,
+    battsm5: Option<f64>,
+    battsm6: Option<f64>,
+    battsm7: Option<f64>,
+    battsm8: Option<f64>,
     co2_in_24h_aqin: Option<f64>,
     co2_in_aqin: Option<f64>,
     dailyrainin: Option<f64>,
@@ -37,6 +53,14 @@ pub struct Report {
     pm25_in_aqin: Option<f64>,
     pm_in_humidity_aqin: Option<f64>,
     pm_in_temp_aqin: Option<f64>,
+    soilhum1: Option<f64>,
+    soilhum2: Option<f64>,
+    soilhum3: Option<f64>,
+    soilhum4: Option<f64>,
+    soilhum5: Option<f64>,
+    soilhum6: Option<f64>,
+    soilhum7: Option<f64>,
+    soilhum8: Option<f64>,
     solarradiation: Option<f64>,
     tempf: Option<f64>,
     tempinf: Option<f64>,
@@ -64,13 +88,13 @@ impl Into<String> for &Report {
 
         if let Some(aqi_pm25) = self.aqi_pm25 {
             metrics.push(format!(
-                "air_quality_pm25_index{{{station}, location=\"pm25\"}} {aqi_pm25} {timestamp}"
+                "air_quality_pm25_index{{{station}, location=\"indoor PM2.5\"}} {aqi_pm25} {timestamp}"
             ));
         }
 
         if let Some(aqi_pm25) = self.aqi_pm25_24h {
             metrics.push(format!(
-                "air_quality_pm25_average_24h_index{{{station}, location=\"pm25\"}} {aqi_pm25} {timestamp}"
+                "air_quality_pm25_average_24h_index{{{station}, location=\"indoor PM2.5\"}} {aqi_pm25} {timestamp}"
             ));
         }
 
@@ -98,9 +122,57 @@ impl Into<String> for &Report {
             ));
         }
 
+        if let Some(battery) = self.batt1 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 1\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt2 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 2\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt3 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 3\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt4 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 4\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt5 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 5\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt6 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 6\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt7 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 7\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.batt8 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"sensor 8\"}} {battery} {timestamp}"
+            ));
+        }
+
         if let Some(battery) = self.batt_25 {
             metrics.push(format!(
-                "battery{{{station}, location=\"pm25\"}} {battery} {timestamp}"
+                "battery{{{station}, location=\"indoor PM2.5\"}} {battery} {timestamp}"
             ));
         }
 
@@ -125,6 +197,54 @@ impl Into<String> for &Report {
         if let Some(battery) = self.battout {
             metrics.push(format!(
                 "battery{{{station}, location=\"weather station\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm1 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 1\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm2 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 2\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm3 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 3\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm4 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 4\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm5 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 5\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm6 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 6\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm7 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 7\"}} {battery} {timestamp}"
+            ));
+        }
+
+        if let Some(battery) = self.battsm8 {
+            metrics.push(format!(
+                "battery{{{station}, location=\"soil humidity 8\"}} {battery} {timestamp}"
             ));
         }
 
@@ -202,13 +322,13 @@ impl Into<String> for &Report {
 
         if let Some(pm25) = self.pm25 {
             metrics.push(format!(
-                "pm25_micrograms_per_cubic_meter{{{station}, location=\"pm25\"}} {pm25} {timestamp}"
+                "pm25_micrograms_per_cubic_meter{{{station}, location=\"indoor PM2.5\"}} {pm25} {timestamp}"
             ));
         }
 
         if let Some(pm25) = self.pm25_24h {
             metrics.push(format!(
-                "pm25_average_24h_micrograms_per_cubic_meter{{{station}, location=\"pm25\"}} {pm25} {timestamp}"
+                "pm25_average_24h_micrograms_per_cubic_meter{{{station}, location=\"indoor PM2.5\"}} {pm25} {timestamp}"
             ));
         }
 
@@ -260,9 +380,57 @@ impl Into<String> for &Report {
             ));
         }
 
+        if let Some(humidity) = self.soilhum1 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 1\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum2 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 2\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum3 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 3\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum4 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 4\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum5 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 5\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum6 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 6\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum7 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 7\"}} {humidity} {timestamp}"
+            ));
+        }
+
+        if let Some(humidity) = self.soilhum8 {
+            metrics.push(format!(
+                "soil_humidity{{{station}, sensor=\"sensor 8\"}} {humidity} {timestamp}"
+            ));
+        }
+
         if let Some(solar_radiation) = self.solarradiation {
             metrics.push(format!(
-                "solar_radiation_watts_per_square_meter{{station}} {solar_radiation} {timestamp}"
+                "solar_radiation_watts_per_square_meter{{{station}}} {solar_radiation} {timestamp}"
             ));
         }
 
@@ -323,10 +491,26 @@ impl TryFrom<&AmbientWeatherReport> for Report {
             aqi_pm25_aqin,
             baromabsin,
             baromrelin,
+            batt1,
+            batt2,
+            batt3,
+            batt4,
+            batt5,
+            batt6,
+            batt7,
+            batt8,
             batt_25,
             batt_co2,
             battin,
             battout,
+            battsm1,
+            battsm2,
+            battsm3,
+            battsm4,
+            battsm5,
+            battsm6,
+            battsm7,
+            battsm8,
             co2_in_24h_aqin,
             co2_in_aqin,
             dailyrainin,
@@ -342,6 +526,14 @@ impl TryFrom<&AmbientWeatherReport> for Report {
             pm25_in_aqin,
             pm_in_humidity_aqin,
             pm_in_temp_aqin,
+            soilhum1,
+            soilhum2,
+            soilhum3,
+            soilhum4,
+            soilhum5,
+            soilhum6,
+            soilhum7,
+            soilhum8,
             solarradiation,
             tempf,
             tempinf,
@@ -372,11 +564,27 @@ impl TryFrom<&AmbientWeatherReport> for Report {
             aqi_pm25_aqin: *aqi_pm25_aqin,
             baromabsin: *baromabsin,
             baromrelin: *baromrelin,
+            batt1: *batt1,
+            batt2: *batt2,
+            batt3: *batt3,
+            batt4: *batt4,
+            batt5: *batt5,
+            batt6: *batt6,
+            batt7: *batt7,
+            batt8: *batt8,
             batt_25: *batt_25,
             batt_co2: *batt_co2,
             batt_lightning: *batt_lightning,
             battin: *battin,
             battout: *battout,
+            battsm1: *battsm1,
+            battsm2: *battsm2,
+            battsm3: *battsm3,
+            battsm4: *battsm4,
+            battsm5: *battsm5,
+            battsm6: *battsm6,
+            battsm7: *battsm7,
+            battsm8: *battsm8,
             co2_in_24h_aqin: *co2_in_24h_aqin,
             co2_in_aqin: *co2_in_aqin,
             dailyrainin: *dailyrainin,
@@ -395,6 +603,14 @@ impl TryFrom<&AmbientWeatherReport> for Report {
             pm25_in_aqin: *pm25_in_aqin,
             pm_in_humidity_aqin: *pm_in_humidity_aqin,
             pm_in_temp_aqin: *pm_in_temp_aqin,
+            soilhum1: *soilhum1,
+            soilhum2: *soilhum2,
+            soilhum3: *soilhum3,
+            soilhum4: *soilhum4,
+            soilhum5: *soilhum5,
+            soilhum6: *soilhum6,
+            soilhum7: *soilhum7,
+            soilhum8: *soilhum8,
             solarradiation: *solarradiation,
             tempf: *tempf,
             tempinf: *tempinf,
