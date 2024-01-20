b7c814e94cfc0b2e68d2e5b680fb45585861ecae
Eric Hodel <drbrain@segment7.net>
2024-01-19 20:48:01 -0800

    Move server stuff into server.rs

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,19 @@ mod info;
 mod metrics;
 mod report;
 mod reports;
+mod server;
 
-use crate::ambient_weather_report::AmbientWeatherReport;
 pub use crate::{gauge::Gauge, info::Info, metrics::Metrics, report::Report, reports::Reports};
-use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
-use std::ops::Deref;
-use tracing::error;
-
-#[get("/")]
-async fn root() -> impl Responder {
-    HttpResponse::NotFound()
-}
-
-#[get("/ambient-weather/upload")]
-async fn ambient_weather_upload(
-    reports: web::Data<Reports>,
-    report: web::Query<AmbientWeatherReport>,
-) -> impl Responder {
-    match report.deref().try_into() {
-        Ok(report) => {
-            reports.add_report(report);
-        }
-        Err(e) => {
-            error!(?e);
-
-            return HttpResponse::BadRequest();
-        }
-    };
-
-    HttpResponse::Ok()
-}
-
-#[get("/metrics")]
-async fn get_metrics(reports: web::Data<Reports>) -> impl Responder {
-    HttpResponse::Ok().body(reports.metrics())
-}
+use server::Server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let reports = web::Data::new(Reports::default());
+    let address = "0.0.0.0:8080".parse().unwrap();
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(reports.clone())
-            .service(root)
-            .service(ambient_weather_upload)
-            .service(get_metrics)
-            .wrap(Logger::default())
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    Server::new(address).start().await?;
+
+    Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,66 @@
+use crate::{ambient_weather_report::AmbientWeatherReport, Reports};
+use actix_web::{
+    get,
+    middleware::Logger,
+    web::{Data, Query},
+    App, HttpResponse, HttpServer, Responder,
+};
+use std::{net::SocketAddr, ops::Deref};
+use tracing::error;
+
+pub struct Server {
+    address: SocketAddr,
+    reports: Data<Reports>,
+}
+
+impl Server {
+    pub fn new(address: SocketAddr) -> Self {
+        Self {
+            address,
+            reports: Data::new(Reports::default()),
+        }
+    }
+
+    pub async fn start(self) -> std::io::Result<()> {
+        HttpServer::new(move || {
+            App::new()
+                .app_data(self.reports.clone())
+                .service(root)
+                .service(ambient_weather_upload)
+                .service(get_metrics)
+                .wrap(Logger::default())
+        })
+        .bind(self.address)?
+        .run()
+        .await
+    }
+}
+
+#[get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::NotFound()
+}
+
+#[get("/ambient-weather/upload")]
+async fn ambient_weather_upload(
+    reports: Data<Reports>,
+    report: Query<AmbientWeatherReport>,
+) -> impl Responder {
+    match report.deref().try_into() {
+        Ok(report) => {
+            reports.add_report(report);
+        }
+        Err(e) => {
+            error!(?e);
+
+            return HttpResponse::BadRequest();
+        }
+    };
+
+    HttpResponse::Ok()
+}
+
+#[get("/metrics")]
+async fn get_metrics(reports: Data<Reports>) -> impl Responder {
+    HttpResponse::Ok().body(reports.metrics())
+}
